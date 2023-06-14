a91f05e36d8219f8fac580da6a7eaf0cd16dd8a9
Aleksandr Trufmanov <kride2009@yandex.ru>
2023-06-15 02:04:29 +0300

    Tests added

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -19,7 +19,59 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void shouldNotPayOverLimit() {
+    public void shouldAddAmountToBalanceHigher0() {
+        CreditAccount account = new CreditAccount(
+                1_000,
+                5_000,
+                15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(4_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddAmountToBalanceLower0() {
+        CreditAccount account = new CreditAccount(
+                -1_000,
+                5_000,
+                15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+     @Test
+    public void shouldThrowExceptionAboutNegativeInitialBalance() {
+        CreditAccount account = new CreditAccount(
+                -1_000,
+                5_000,
+                15
+        );
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            account.add(3_000);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionAboutNegativeCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                -5_000,
+                15
+        );
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            account.add(3_000);
+        });
+    }
+
+    @Test
+    public void shouldReturnFalseBecausePayOverLimit() {
         CreditAccount account = new CreditAccount(
                 0,
                 5_000,
@@ -28,7 +80,7 @@ public class CreditAccountTest {
 
         account.pay(6_000);
 
-        Assertions.assertEquals(-6_000, account.getBalance());
+        Assertions.assertEquals(0, account.getBalance());
     }
 
     @Test
@@ -44,16 +96,4 @@ public class CreditAccountTest {
         Assertions.assertEquals(1_500, account.getBalance());
     }
 
-    @Test
-    public void shouldThrowExceptionAboutNegativeRate() {
-        CreditAccount account = new CreditAccount(
-                0,
-                5_000,
-                -15
-        );
-
-        account.add(3_000);
-
-        Assertions.assertEquals(3_000, account.getBalance());
-    }
 }
