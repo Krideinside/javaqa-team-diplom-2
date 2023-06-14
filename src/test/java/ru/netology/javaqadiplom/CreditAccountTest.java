6840011c90a70b9c014f60c55cccbac77e8c16f1
Aleksandr Trufmanov <kride2009@yandex.ru>
2023-06-15 01:19:15 +0300

    Added new test

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,43 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    @Test
+    public void shouldNotPayOverLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.pay(6_000);
+
+        Assertions.assertEquals(-6_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldReduceBalance() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                15
+        );
+
+        account.pay(500);
+
+        Assertions.assertEquals(1_500, account.getBalance());
+    }
+
+    @Test
+    public void shouldThrowExceptionAboutNegativeRate() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                -15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(3_000, account.getBalance());
+    }
 }
